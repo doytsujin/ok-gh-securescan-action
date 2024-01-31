6b4d206bbb456836c55deb45009b09941d3495df
chelex <chelex@nostromo.phub.net.cable.rogers.com>
2024-01-31 02:04:09 +0000

    * tested with org

--- src/main.rs
+++ src/main.rs
@@ -120,7 +120,8 @@ async fn run(enterprise: &str, rate_limiter: &Ratelimiter) {
             Ok(_) => {
                 let request_builder = client
                     .get(&format!(
-                        "https://api.github.com/repos/{}/ok-gh-securescan-action",
+                        "https://api.github.com/repos/{}/ok-gh-securescan-test",
+                        // "https://api.github.com/repos/{}/ok-gh-securescan-action",
                         // "https://api.github.com/enterprises/{}/repos",
                         enterprise
                     ))
